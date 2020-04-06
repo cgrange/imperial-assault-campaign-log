b0fdbdddd73ea9089fee7b8da5760535062420cc
Colton Grange <colton.grange@gm.com>
2020-04-06 17:07:38 -0500

    add testDB route add mongodb and try to connect to mongo atlas cluster

--- api/app.js
+++ api/app.js
@@ -4,12 +4,40 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const testApiRouter = require('./routes/testApi');
+const testDBRouter = require('./routes/testDB');
+require('dotenv/config');
 
 var app = express();
+
+const client = new MongoClient(process.env.MONGODB_CONNECTION, {useUnifiedTopology: true});
+async function run() {
+  try {
+    await client.connect();
+    console.log("DB connected!");
+    const db = client.db("testdb");
+    const collection = db.collection("people");
+    let document = {
+      name: "colton",
+      title: "bmf",
+      role: "software engineer",
+      weaknesses: "switchblade"
+    };
+    await collection.insertOne(document);
+    const person = await collection.findOne();
+    console.log(person);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +53,7 @@ app.use(cors({origin: 'http://localhost:3000'}));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testApi', testApiRouter);
+app.use('/testDB', testDBRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
